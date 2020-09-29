6e1f516c018c1acdeaade46b9cdeb4bd2cbd50be
micahm25 <70343313+micahm25@users.noreply.github.com>
2020-09-29 11:47:22 -1000

    Labs

---
+++ Lab7/ex4.js
@@ -0,0 +1,9 @@
+name = "Micah";
+likesbeer = "false";
+//document.write(name + ' does ');
+if(likesbeer == false){
+    document.write('Beer is something I dislike!');
+}
+else {
+    document.write('I like beer!');
+}
---
+++ Lab8/ex1.js
@@ -0,0 +1,7 @@
+age_count = 1; 
+my_age = 21;
+while(age_count < my_age){
+    console.log(`age ${age_count}`);
+    age_count++;
+}
+console.log(`Micah is ${age_count} years old!`)
