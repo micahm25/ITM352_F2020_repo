8415bb6b33912d3ac50eaf36145560fd7f914e0b
micahm25 <70343313+micahm25@users.noreply.github.com>
2020-10-06 03:03:43 -1000

    wods

---
+++ Lab9/ex1.js
@@ -0,0 +1,4 @@
+student = ['ITM352', 'BUS311', 'ACC101', 'FIN100', 'BUS310', 'MOM202'];
+student[2]='ITM353';
+student.push('MKT300');
+console.log(student);
---
+++ Lab9/ex2.js
@@ -0,0 +1,3 @@
+student = {firstName:'Dan', lastName:'Kazman', major:'MIS', year:'3', id:'123456789', onProbation:'true'};
+
+console.log(student.major);
